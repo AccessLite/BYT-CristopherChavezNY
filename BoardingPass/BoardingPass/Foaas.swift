f8bac22deb2947c7cd9e7bcfd87140e12e4b3392
Cristopher Chavez <cristopherchavezny@gmail.com>
2016-11-25 15:04:31 -0500

    Tested APIManager functions, they are working as intended

---
+++ BoardingPass/BoardingPass/Foaas.swift
@@ -0,0 +1,56 @@
+//
+//  Foaas.swift
+//  BoardingPass
+//
+//  Created by Cris on 11/23/16.
+//  Copyright © 2016 Cris. All rights reserved.
+//
+
+import Foundation
+
+enum FoaasErrorHandler: Error {
+    case jsonError, message, subtitle
+}
+/*
+ // Enpoint: /awesome/:from
+ // GET http://www.foaas.com/awesome/louis
+ {
+ "message": "This is Fucking Awesome.",
+ "subtitle": "- louis"
+ }
+ */
+struct Foaas: JSONConvertible, CustomStringConvertible {
+    let message: String
+    let subtitle: String
+    var description : String {
+        return "\(message) \(subtitle)"
+    }
+    
+    init?(json: [String : Any]) {
+        do {
+            guard let message = json["message"] as? String else {throw FoaasErrorHandler.message}
+            guard let subtitle = json["subtitle"] as? String else {throw FoaasErrorHandler.subtitle}
+            self.message = message
+            self.subtitle = subtitle
+        }
+        catch FoaasErrorHandler.message {
+            print("ERROR WITH PARSING MESSAGE")
+        }
+        catch FoaasErrorHandler.subtitle {
+            print("ERROR WITH PARSING SUBTITLE")
+        }
+        catch {
+            print("PRINT \(error)")
+        }
+        return nil
+    }
+    
+    func toJson() -> [String : Any] {
+        let fooasDict: [String : Any] = [
+            "message" : self.message,
+            "subtitle" :self.subtitle,
+            "description" : self.description
+        ]
+        return fooasDict
+    }
+}
