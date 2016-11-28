1ae93b215dd76b687434188369b9011360aa940c
Cristopher Chavez <cristopherchavezny@gmail.com>
2016-11-27 23:58:21 -0500

    Added text fields to previewViewController

---
+++ BoardingPass/BoardingPass/PreviewViewController.swift
@@ -0,0 +1,149 @@
+//
+//  PreviewViewController.swift
+//  BoardingPass
+//
+//  Created by Cris on 11/27/16.
+//  Copyright © 2016 Cris. All rights reserved.
+//
+
+import UIKit
+
+class PreviewViewController: UIViewController, UITextFieldDelegate {
+    
+    var operation: FoaasOperation!
+    
+    lazy var previewLabel: UILabel = {
+        let pl = UILabel()
+        pl.translatesAutoresizingMaskIntoConstraints = false
+        pl.font = UIFont(name: "HelveticaNeue-BoldItalic", size: 20)
+        pl.text = "Preview:"
+        return pl
+    }()
+    
+    lazy var nameLabel: UILabel = {
+        let pl = UILabel()
+        pl.translatesAutoresizingMaskIntoConstraints = false
+        pl.font = UIFont.systemFont(ofSize: 20)
+        pl.text = "<name>"
+        return pl
+    }()
+    
+    lazy var fromLabel: UILabel = {
+        let pl = UILabel()
+        pl.translatesAutoresizingMaskIntoConstraints = false
+        pl.font = UIFont.systemFont(ofSize: 20)
+        pl.text = "<from>"
+        return pl
+    }()
+    
+    lazy var referenceLabel: UILabel = {
+        let pl = UILabel()
+        pl.translatesAutoresizingMaskIntoConstraints = false
+        pl.font = UIFont.systemFont(ofSize: 20)
+        pl.text = "<reference>"
+        return pl
+    }()
+    
+    lazy var previewTextView: UITextView = {
+        let ptv = UITextView()
+        ptv.translatesAutoresizingMaskIntoConstraints = false
+        ptv.isEditable = false
+        ptv.isSelectable = false
+        ptv.sizeToFit()
+        return ptv
+    }()
+    
+    lazy var textFieldOne: UITextField = {
+        let tf = UITextField()
+        tf.delegate = self
+        tf.borderStyle = .roundedRect
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.placeholder = "Name"
+        return tf
+    }()
+    
+    lazy var textFieldTwo: UITextField = {
+        let tf = UITextField()
+        tf.delegate = self
+        tf.borderStyle = .roundedRect
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.placeholder = "from"
+        return tf
+    }()
+
+    lazy var textFieldThree: UITextField = {
+        let tf = UITextField()
+        tf.delegate = self
+        tf.borderStyle = .roundedRect
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.placeholder = "reference"
+        return tf
+    }()
+
+    func setUpViews() {
+        view.addSubview(previewLabel)
+        view.addSubview(previewTextView)
+        view.addSubview(nameLabel)
+        view.addSubview(textFieldOne)
+        view.addSubview(fromLabel)
+        view.addSubview(textFieldTwo)
+        view.addSubview(referenceLabel)
+        view.addSubview(textFieldThree)
+        
+        previewLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 75).isActive = true
+        previewLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 8).isActive = true
+        previewLabel.rightAnchor.constraint(equalTo: view.rightAnchor,  constant: -8).isActive = true
+        
+        previewTextView.rightAnchor.constraint(equalTo:  view.rightAnchor).isActive = true
+        previewTextView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        previewTextView.topAnchor.constraint(equalTo: previewLabel.bottomAnchor).isActive = true
+        previewTextView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: CGFloat(0.25)).isActive = true
+        
+        nameLabel.topAnchor.constraint(equalTo: previewTextView.bottomAnchor, constant: 20).isActive = true
+        nameLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 8).isActive = true
+        nameLabel.rightAnchor.constraint(equalTo: view.rightAnchor,  constant: -8).isActive = true
+
+        
+        textFieldOne.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 10).isActive = true
+        textFieldOne.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -10).isActive = true
+        textFieldOne.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 10).isActive = true
+        
+        fromLabel.topAnchor.constraint(equalTo: textFieldOne.bottomAnchor, constant: 20).isActive = true
+        fromLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 8).isActive = true
+        fromLabel.rightAnchor.constraint(equalTo: view.rightAnchor,  constant: -8).isActive = true
+    
+        
+        textFieldTwo.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 10).isActive = true
+        textFieldTwo.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -10).isActive = true
+        textFieldTwo.topAnchor.constraint(equalTo: fromLabel.bottomAnchor, constant: 10).isActive = true
+        
+        referenceLabel.topAnchor.constraint(equalTo: textFieldTwo.bottomAnchor, constant: 20).isActive = true
+        referenceLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 8).isActive = true
+        referenceLabel.rightAnchor.constraint(equalTo: view.rightAnchor,  constant: -8).isActive = true
+        
+        textFieldThree.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 10).isActive = true
+        textFieldThree.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -10).isActive = true
+        textFieldThree.topAnchor.constraint(equalTo: referenceLabel.bottomAnchor, constant: 10).isActive = true
+
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        createSelectButton()
+        setUpViews()
+        previewTextView.text = operation.urlString
+        // Do any additional setup after loading the view.
+    }
+    
+    func createSelectButton() {
+        self.title = operation.name
+        let rightButton = UIBarButtonItem(title: "Select", style: UIBarButtonItemStyle.done, target: self, action: #selector(handleSelectButton))
+        navigationItem.rightBarButtonItem = rightButton
+    }
+    func handleSelectButton() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+}
